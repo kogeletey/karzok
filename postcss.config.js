a769d28662368278e9bd81081a4258cde9e84417
kogeletey <kg@re128.org>
2021-12-13 09:30:18 +0300

    root: package.json,postcss.config.js remove purgecss plugin, as it does not work correctly. sass: _base.scss moved root colors css to main.scss

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,5 @@
-//import purgecss from '@fullhuman/postcss-purgecss';
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
     plugins: [
-        require('autoprefixer'),
-        purgecss({
-            content: ['./**/*.html,', './**/*.js'],
-            css: ['./**/*.css'],
-        }),
+        require('autoprefixer')
     ],
 };
