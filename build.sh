8a872315b0c0babb6c234c93656e38f653ec6830
kogeletey <kg@re128.org>
2022-04-28 22:01:53 +0300

    root: build.sh adding copy folder static,content,templates to www and change directory zola build

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
+DIRECTORY=("content" "templates" "static")
+FILE="config.toml"
+
+for i in "${DIRECTORY[@]}"; do
+  echo $i
+  if [[ -d "$i" ]] && [[ -f "$FILE" ]]; then
+     cp -rf $i $FILE /www
+  fi
+done
+
 if [ $1 ]; then
+  cd /www
   zola build -u $1
 else
+  cd /www
   zola build
 fi
-
-
-
