95b0c0ffe990e3a45470d0319348e6f9cea79a2a
kogeletey <kg@re128.org>
2022-06-10 21:28:25 +0300

    helpers: fixed checkbox and adding listener. sass: change variables to background and foreground

--- helpers/mode-switch.ts
+++ helpers/mode-switch.ts
@@ -5,9 +5,10 @@ function set_theme(mode:string) {
     }
     document.body.dataset.theme = mode
     localStorage.setItem("theme", mode)
-    console.log(`change to ${mode} mode`)
+//    console.log(`change to ${mode} mode`)
 }
 
+function get_theme() {
 const current_theme = localStorage.getItem("theme") || "auto"
 const prefers_dark = window.matchMedia("(prefers-color-scheme: dark)").matches
 
@@ -15,16 +16,22 @@ if (prefers_dark) {
     if (current_theme == "auto") {
         set_theme("light")
     } else if (current_theme == "light") {
-        set_theme("dark")
+        set_theme("black")
     } else {
         set_theme("auto")
     }
   } else {
     if (current_theme == "auto") {
-        set_theme("dark")
-    } else if (current_theme == "light") {
+        set_theme("black")
+    } else if (current_theme == "black") {
         set_theme("light")
     } else {
         set_theme("auto")
     }
+  }
 }
+
+const btn = document.getElementsByClassName("toggle")
+Array.from(btn).forEach((button => {
+        button.addEventListener("click", get_theme)
+}))
