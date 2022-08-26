dbdd83fd6004cdbf3d642e31103b276513963f71
kogeletey <kg@re128.org>
2022-08-26 18:39:57 +0300

    feat: base a config.toml filled with surveys.

--- dist/ctl/main.go
+++ dist/ctl/main.go
@@ -10,6 +10,9 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/BurntSushi/toml"
 )
 
 func DownloadPackage(url string) ([]byte, error) {
@@ -102,4 +105,67 @@ func main() {
 		log.Printf("failed get package", err)
 	}
 	Untar(file, "themes/karzok")
+
+	var qs = []*survey.Question{
+		{
+			Name:   "title",
+			Prompt: &survey.Input{Message: "Enter a title of your site (Default: Karzok)"},
+		},
+		{
+			Name:   "base_url",
+			Prompt: &survey.Input{Message: "Enter a url of your site (Default: http://localhost:8080)"},
+		},
+		{
+			Name:   "description",
+			Prompt: &survey.Input{Message: "Enter a description of your site"},
+		},
+		{
+			Name:   "build_search_index",
+			Prompt: &survey.Confirm{Message: "Do you want to build a search index of the content?"},
+		},
+		{
+			Name:   "default_language",
+			Prompt: &survey.Input{Message: "Enter a language of your site"},
+		},
+		{
+			Name:   "compile_sass",
+			Prompt: &survey.Confirm{Message: "Do you want to enable Sass compilation?"},
+		},
+	}
+
+	type Answers struct {
+		Title            string `toml:"title"`
+		BaseUrl          string `toml:"base_url" survey:"base_url"`
+		Description      string `toml:"description"`
+		DefaultLanguage  string `toml:"default_language" survey:"default_language"`
+		BuildSearchIndex bool   `toml:"build_search_index" survey:"build_search_index"`
+		CompileSass      bool   `toml:"compile_sass" survey:"compile_sass"`
+	}
+
+	var answers Answers
+
+	survey.Ask(qs, &answers)
+
+	if answers.Title == "" {
+		answers.Title = "Karzok"
+	}
+	if answers.BaseUrl == "" {
+		answers.BaseUrl = "http://localhost:8080"
+	}
+
+	if answers.DefaultLanguage == "" {
+		answers.DefaultLanguage = "en"
+	}
+
+	file, err := os.Create("config.toml")
+	if err != nil {
+		log.Fatal("failed create config.toml", err)
+	}
+
+	defer file.Close()
+
+	err = toml.NewEncoder(file).Encode(&answers)
+	if err != nil {
+		log.Fatal("failed create config.toml", err)
+	}
 }
